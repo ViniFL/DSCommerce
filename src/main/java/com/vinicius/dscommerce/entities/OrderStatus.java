d2f7f1161874a01f9a8d88bf069b4bf186bd80c1
Vinicius Fernandes <vinicius.fernandes.lima@outlook.com>
2023-08-09 00:28:40 -0300

    Order e OrderStatus criado

---
+++ src/main/java/com/vinicius/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.vinicius.dscommerce.entities;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+}
+
--- src/main/java/com/vinicius/dscommerce/entities/User.java
+++ src/main/java/com/vinicius/dscommerce/entities/User.java
@@ -3,6 +3,8 @@ package com.vinicius.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -16,6 +18,9 @@ public class User {
     private String phone;
     private LocalDate birthDate;
     private String Password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -75,5 +80,9 @@ public class User {
 
     public void setPassword(String password) {
         Password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 }
