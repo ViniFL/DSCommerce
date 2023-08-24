1b7708234a29d94bec6075030045d2936aa99fec
Vinicius Fernandes <vinicius.fernandes.lima@outlook.com>
2023-08-24 07:26:35 -0300

    PUT product

--- src/main/java/com/vinicius/dscommerce/controllers/ProductController.java
+++ src/main/java/com/vinicius/dscommerce/controllers/ProductController.java
@@ -3,7 +3,7 @@ package com.vinicius.dscommerce.controllers;
 import com.vinicius.dscommerce.dto.ProductDTO;
 
 import com.vinicius.dscommerce.services.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;git 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -36,5 +36,11 @@ public class ProductController {
         dto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
     }
 }
